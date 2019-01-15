8f49e079b23cda6eb6671872997f5378c270154c
Charles Patterson Cook <cook.char.p@gmail.com>
2019-01-15 11:49:40 -0500

    Added drawGrid() and 3/7 blocks in drawPiece(y, x, o, p, w)

--- cursatetra.py
+++ cursatetra.py
@@ -2,6 +2,64 @@
 import curses as crs
 
 #SECTION: FUNCTIONS
+"""
+Draws an alignment grid in the board window
+"""
+def drawGrid():
+	for x in range(1, 21):
+		for y in range(1, 23):
+			wBoard.addch(y, x, '.')
+
+"""
+# Block designations
+
+* C : Square
+
+* S : S-piece
+* Z : Z-piece
+* L : L-piece
+* R : Reversed L-piece
+* I : Line
+* T : T-piece
+
+# Orientations
+
+* H : horizontal
+* V : vertical
+* HP : horizontal, pi radians around (180 deg)
+* VP : vertical, pi radians around (180 deg)
+"""
+def drawPiece(y, x, orient, piece, window):
+	if piece == 'C':
+		for i in range(x, x + 4):
+			for j in range(y, y + 2):
+				window.addch(j, i, crs.ACS_CKBOARD)
+	elif piece == 'S':
+		if orient == 'H':
+			for i in range(x + 2, x + 6):
+				window.addch(y, i, crs.ACS_CKBOARD)
+			for i in range(x, x + 4):
+				window.addch(y + 1, i, crs.ACS_CKBOARD)
+		else:
+			for i in range(x, x + 2):
+				for j in range(y, y + 2):
+					window.addch(j, i, crs.ACS_CKBOARD)
+			for i in range(x + 2, x + 4):
+				for j in range(y + 1, y + 3):
+					window.addch(j, i, crs.ACS_CKBOARD)
+	elif piece == 'Z':
+		if orient == 'H':
+			for i in range(x, x + 4):
+				window.addch(y, i, crs.ACS_CKBOARD)
+			for i in range(x + 2, x + 6):
+				window.addch(y + 1, i, crs.ACS_CKBOARD)
+		else:
+			for i in range(x, x + 2):
+				for j in range(y + 1, y + 3):
+					window.addch(j, i, crs.ACS_CKBOARD)
+			for i in range(x + 2, x + 4):
+				for j in range(y, y + 2):
+					window.addch(j, i, crs.ACS_CKBOARD)
 
 #SECTION: MAIN
 #Initialize screen
@@ -29,7 +87,9 @@ wTitle.addstr(1, 4, "Cursa Tetra")
 wTitle.addstr(2, 1, 17 * '-')
 wTitle.addstr(3, 2, "By Charlie Cook")
 wScore.addstr(1, 1, "SCORE:")
+drawGrid()
 wNextP.addstr(1, 1, "NEXT PIECE:")
+drawPiece(2, 3, 'V', 'Z', wNextP)
 #Make windows visible
 wTitle.refresh()
 wScore.refresh()
