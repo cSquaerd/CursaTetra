7c78677e9216a7572da797176cd7f014ee3078fc
Charles Patterson Cook <cook.char.p@gmail.com>
2019-01-15 01:28:45 -0500

    Added next block window

--- cursatetra.py
+++ cursatetra.py
@@ -16,22 +16,26 @@ crs.curs_set(0)
 wTitle = crs.newwin(6, 19, 0, 4)
 wScore = crs.newwin(4, 17, 8, 5)
 wCntrl = crs.newwin(10, 23, 12, 2)
-wBoard = crs.newwin(24, 22, 0, 34)
+wBoard = crs.newwin(24, 22, 0, 28)
+wNextP = crs.newwin(6, 13, 4, 52)
 #Draw boarders of windows
 wTitle.border()
 wScore.border()
 wCntrl.border()
 wBoard.border()
+wNextP.border()
 #Write titles and labels
 wTitle.addstr(1, 4, "Cursa Tetra")
 wTitle.addstr(2, 1, 17 * '-')
 wTitle.addstr(3, 2, "By Charlie Cook")
 wScore.addstr(1, 1, "SCORE:")
+wNextP.addstr(1, 1, "NEXT PIECE:")
 #Make windows visible
 wTitle.refresh()
 wScore.refresh()
 wCntrl.refresh()
 wBoard.refresh()
+wNextP.refresh()
 
 #Wait
 crs.delay_output(2000)
